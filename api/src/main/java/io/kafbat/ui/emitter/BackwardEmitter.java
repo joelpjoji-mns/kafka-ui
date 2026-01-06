d791e3e2bffe642a1def56c8ad0c319706ff6f43
Joel P Joji <joel.joji@mnscorp.net>
2026-01-06 08:34:16 -0500

    feat: update BackwardEmitter.java

--- api/src/main/java/io/kafbat/ui/emitter/BackwardEmitter.java
+++ api/src/main/java/io/kafbat/ui/emitter/BackwardEmitter.java
@@ -24,7 +24,7 @@ public class BackwardEmitter extends RangePollingEmitter {
         consumerSupplier,
         consumerPosition,
         messagesPerPage,
-        new MessagesProcessing(deserializer, filter, false, messagesPerPage),
+        MessagesProcessing.create(deserializer, filter, false, messagesPerPage),
         pollingSettings,
         cursor
     );
