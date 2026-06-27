fb7335f32656351755f4eae2781390255af4218f
Joel P Joji <joel.joji@mnscorp.net>
2026-06-26 20:01:55 -0500

    feat: update ControlPanel.styled.ts

--- frontend/src/components/common/ControlPanel/ControlPanel.styled.ts
+++ frontend/src/components/common/ControlPanel/ControlPanel.styled.ts
@@ -8,6 +8,7 @@ export const ControlPanelWrapper = styled.div<Props>`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  flex-wrap: wrap;
   padding: 0 16px;
   margin: 0 0 16px;
   width: 100%;
@@ -15,5 +16,16 @@ export const ControlPanelWrapper = styled.div<Props>`
   color: ${({ theme }) => theme.default.color.normal};
   & > *:first-child {
     width: ${(props) => (props.hasInput ? '38%' : 'auto')};
+    min-width: 0;
+  }
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.M}px) {
+    flex-direction: column;
+    align-items: stretch;
+    gap: 8px;
+
+    & > *:first-child {
+      width: 100%;
+    }
   }
 `;
