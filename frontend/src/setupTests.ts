6ca7796e14ee754960d057a7ac61fb0696a221e6
Joel P Joji <joel.joji@mnscorp.net>
2026-08-08 21:12:42 -0500

    test: update setupTests.ts

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,3 +1,7 @@
 import 'whatwg-fetch';
 import 'jest-styled-components';
 import '@testing-library/jest-dom/jest-globals';
+import { TextDecoder, TextEncoder } from 'util';
+
+global.TextEncoder = TextEncoder as typeof global.TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
